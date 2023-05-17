89c5812075635ac3bef8113f6dcd2c50986efee9
Maximiliano <mavzdev@gmail.com>
2023-05-17 14:59:15 -0300

    agregue de todo xd

---
+++ js/constructores.js
@@ -0,0 +1,13 @@
+class Reserva {
+    nombre;
+    numero;
+    entrada;
+    salida;
+
+    constructor (nombre, numero, entrada, salida) {
+        this.nombre = nombre;
+        this.numero = numero;
+        this.entrada = entrada;
+        this.salida = salida;
+    }
+}
